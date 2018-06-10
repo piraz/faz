28501f77194bbd934b26ce2320925e60b6758276
Flavio Garcia <piraz@candango.org>
2018-06-09 23:11:02 -0400

    Fixed references to the alert component.
    Fixes: #7

--- alert/alert.js
+++ alert/alert.js
@@ -1,11 +1,11 @@
 "use strict";
 "format cjs";
 
-var FazAlertViewModel = require("./faz-alert-view-model");
+var FazAlertViewModel = require("./alert-view-model");
 var Component = require("can-component");
 
 //import template from "./nav.stache";
-var alertTemplate = require("./faz-alert.stache");
+var alertTemplate = require("./alert.stache");
 
 //import canMap from "can-connect/can/map/map";
 //import connect from "can-connect";
