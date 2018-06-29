c4bed38ca53a822e119ace1dbc7c652239a7eda9
Flavio Garcia <piraz@candango.org>
2018-06-29 00:49:26 -0400

    Changed navbar to use it's main file.
    Also added the showcase.less to the alert and nav mains.
    
    Refs: #21

--- app/alert.js
+++ app/alert.js
@@ -1,2 +1,6 @@
+"use strict";
+"format cjs";
+
+require("../stylesheets/showcase.less");
 require("../alert/alert");
 require("../nav/nav");
--- app/nav.js
+++ app/nav.js
@@ -1,2 +1,6 @@
+"use strict";
+"format cjs";
+
+require("../stylesheets/showcase.less");
 require("../nav/nav");
 require("../alert/alert");
