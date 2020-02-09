417ac653bb1ee835f1880d5ad950151415fb3514
Flavio Garcia <piraz@candango.org>
2020-02-09 00:21:03 -0500

    Removed left over console.log.
    Added header comment to files.

--- item.js
+++ item.js
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2018-2020 Flavio Garcia
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import { default as ID } from "./id";
 import { ObservableObject, type } from "can";
 import DeepObservable from "can-deep-observable";
@@ -25,7 +41,6 @@ class FazItem extends ObservableObject {
             href: String,
             parent: "*",
             get isLink() {
-                console.log(this.href);
                 return this.href !== undefined;
             }
         };
