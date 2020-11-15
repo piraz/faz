f8de5fc005cf3f954e154a01659b77f698077a19
Flavio Garcia <piraz@candango.org>
2020-11-15 01:21:36 -0500

    Fixed missing import of ObservableArray.

--- form/form.js
+++ form/form.js
@@ -15,7 +15,7 @@
  */
 
 import { default as ID } from "../id";
-import { DeepObservable, StacheElement, type } from "can";
+import { DeepObservable, ObservableArray, StacheElement, type } from "can";
 
 
 export default class FazForm extends StacheElement {
